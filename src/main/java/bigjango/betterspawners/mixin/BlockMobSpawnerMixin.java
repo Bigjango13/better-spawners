4307da8b00a0e97c20cc657606924740269d8c57
Apollointhehouse <99405787+Apollointhehouse@users.noreply.github.com>
2023-12-19 18:15:42 +1300

    Spawners now read off NBT when placed

--- src/main/java/bigjango/betterspawners/mixin/BlockMobSpawnerMixin.java
+++ src/main/java/bigjango/betterspawners/mixin/BlockMobSpawnerMixin.java
@@ -1,5 +1,8 @@
 package bigjango.betterspawners.mixin;
 
+import net.minecraft.core.entity.EntityLiving;
+import net.minecraft.core.entity.player.EntityPlayer;
+import net.minecraft.core.util.helper.Side;
 import net.minecraft.core.world.World;
 import net.minecraft.core.block.entity.TileEntityMobSpawner;
 import net.minecraft.core.enums.EnumDropCause;
@@ -9,6 +12,8 @@ import net.minecraft.core.block.Block;
 import com.mojang.nbt.CompoundTag;
 import net.minecraft.core.item.ItemStack;
 
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,15 +21,30 @@ import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(value = BlockMobSpawner.class, remap = false)
 public abstract class BlockMobSpawnerMixin {
+    @Shadow public abstract TileEntity getNewBlockEntity();
+
     @Inject(method = "getBreakResult", at = @At(value = "HEAD"), cancellable = true)
     public void getBreakResult(World world, EnumDropCause dropCause, int x, int y, int z, int meta, TileEntity tileEntity, CallbackInfoReturnable<ItemStack[]> cir) {
         if (dropCause != EnumDropCause.PICK_BLOCK) return;
         ItemStack stack = new ItemStack(Block.mobspawner);
+
         if (tileEntity != null) {
             CompoundTag compound = new CompoundTag();
-            ((TileEntityMobSpawner) tileEntity).writeToNBT(compound);
+            tileEntity.writeToNBT(compound);
             stack.getData().putCompound("SpawnerData", compound);
         }
         cir.setReturnValue(new ItemStack[]{stack});
+    }
+
+    @Inject(method = "onBlockPlaced", at = @At("TAIL"))
+    public void onBlockPlaced(World world, int x, int y, int z, Side side, EntityLiving entity, double sideHeight, CallbackInfo ci) {
+        EntityPlayer player = (EntityPlayer)entity;
+        CompoundTag spawnerData = player.getHeldItem().getData().getCompoundOrDefault("SpawnerData", null);
+        TileEntityMobSpawner tileEntity = (TileEntityMobSpawner)world.getBlockTileEntity(x, y, z);
+        if (tileEntity == null) tileEntity = (TileEntityMobSpawner)getNewBlockEntity();
+        if (spawnerData == null) return;
+
+        tileEntity.setMobId(spawnerData.getString("EntityId"));
+        tileEntity.delay = spawnerData.getShort("Delay");
     }
 }
